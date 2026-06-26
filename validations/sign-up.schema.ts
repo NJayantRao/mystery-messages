af0ef5dff89eb6195908892c4eaa30b6c1798f67
N Jayant Rao <njayantrao@gmail.com>
2026-06-26 19:33:11 +0530

    feat: Add zod validations

---
+++ validations/sign-up.schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+  username: z
+    .string()
+    .min(2, "Username must be at least 2 characters long")
+    .max(20, "Username must be at most 20 characters long")
+    .regex(
+      /^[a-zA-Z0-9_]+$/,
+      "Username can only contain letters, numbers, and underscores",
+    ),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+});
