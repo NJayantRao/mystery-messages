82edef8b967d96472ca2ea9aae9c1b4b0bf8b96d
N Jayant Rao <njayantrao@gmail.com>
2026-06-28 18:28:26 +0530

    chore: fix proxy.ts verify-code bug

--- proxy.ts
+++ proxy.ts
@@ -10,13 +10,20 @@ export async function proxy(request: NextRequest) {
     token &&
     (url.pathname.startsWith("/sign-in") ||
       url.pathname.startsWith("/sign-up") ||
-      url.pathname.startsWith("/verify") ||
+      url.pathname.startsWith("/verify-code") ||
       url.pathname === "/")
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/", "/sign-in", "/sign-up", "/dashboard/:path*", "/verify/:path*"],
+  matcher: [
+    "/",
+    "/sign-in",
+    "/sign-up",
+    "/dashboard/:path*",
+    "/verify-code/:path*",
+  ],
 };
